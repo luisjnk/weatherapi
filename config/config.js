f066446e2f013d538d1df048d0d093c1c2beb855
Luis Junqueira | Stone <ljunqueira@stone.com.br>
2017-06-12 14:02:00 -0300

    Changing connection string

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'databaseDev':'mongodb://localhost/galactus',
+    'databaseDev':'mongodb://luisjnk:lnn4138@ds161551.mlab.com:61551/galactus',
      databaseProd :'mongodb://luisjnk:lnn4138@ds033123.mongolab.com:33123/testapi'
 };
--- config/gamesapiconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'key': 'EoONAESd8Qmsh6Hihz3G3daR9i5Rp157UVYjsngCSBdjbm2ECB',
-    'url':'https://igdbcom-internet-game-database-v1.p.mashape.com/games/',
-    'res' :'?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search='
-};
-
